a4f992418ef4393f9f7d21d53a00b48fd0c213a0
nehaejaz <neha.ejaz@icloud.com>
2019-05-29 02:17:24 +0500

    Counter for splash

--- components/Splash.js
+++ components/Splash.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react'
 import {StyleSheet, Text , View} from 'react-native'
 
 class Splash extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            timer: 0
+        }
+        setInterval(()=>{
+            this.setState({timer: this.state.timer + 1})
+        },1000)
+    }
     render(){
         return(
             <View style={styles.container}>
-            <Text style={styles.title}>Hello</Text>
+            <Text style={styles.title}>Hello{this.state.timer}</Text>
             </View>
         )
     }
