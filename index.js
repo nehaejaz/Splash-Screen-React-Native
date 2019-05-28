a3ce72e2e2e316e9828ee4fac3ec2434c521cee7
nehaejaz <neha.ejaz@icloud.com>
2019-05-29 02:16:38 +0500

    render the main componet for splash screen

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 /**
  * @format
  */
-
+import React, {Component} from 'react'
 import {AppRegistry} from 'react-native';
 import Splash from './components/Splash';
 import {name as appName} from './app.json';
 import Login from './components/Login';
+import MainComponent from './components/MainComponent'
+// import. console = require('console');
 
-AppRegistry.registerComponent(appName, () => Login);
+// class MainComponent extends Component {
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             currentScreen:"splash",
+//         };
+//         setTimeout(()=>{
+// console.log("3 sec tasks")
+// this.setState({currentScreen:"login"})
+//         },3000)
+//     }
+//     render(){
+//         const {currentScreen} = this.state  
+//         let mainScreen= currentScreen==="splash" ? <Splash /> : <Login/>
+//         return mainScreen
+//     }
+// }
+AppRegistry.registerComponent(appName, () => MainComponent);
