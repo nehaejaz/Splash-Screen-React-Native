66971cb4853baeb606a1e33b213e503a822e76d6
nehaejaz <neha.ejaz@icloud.com>
2019-05-29 11:48:55 +0500

    Modified with library functions

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,22 +1,33 @@
 import React, {Component} from 'react'
 import Splash from './Splash'
 import Login from './Login'
+import SplashScreen from 'react-native-splash-screen'
+
 
 class MainComponent extends Component {
+
+    componentDidMount() {
+      SplashScreen.hide();
+    }
     constructor(props){
         super(props);
         this.state={
             currentScreen:"splash",
         };
-        setTimeout(()=>{
-console.log("3 sec tasks")
-this.setState({currentScreen:"login"})
-        },3000)
+//         setTimeout(()=>{
+// console.log("3 sec tasks")
+// this.setState({currentScreen:"login"})
+//         },3000)
+
     }
     render(){
-        const {currentScreen} = this.state  
-        let mainScreen= currentScreen==="splash" ? <Splash /> : <Login/>
-        return mainScreen
+        // const {currentScreen} = this.state  
+        // let mainScreen= currentScreen==="splash" ? <Splash /> : <Login/>
+        // return mainScreen
+        return(
+<Login />
+        )
+        
     }
 }
 
